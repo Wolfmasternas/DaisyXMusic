820fc3c15d3fc187ce8bbc170270990cfa91d07a
Wolfmasternas <98648610+Wolfmasternas@users.noreply.github.com>
2022-02-23 23:25:19 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -1,6 +1,25 @@
-from os import getenv
+# This file is part of Daisy (Telegram Bot)
 
-py -m pip install # sit module in cmd
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+import yaml
+from envparse import env
+
+from DaisyX.utils.logger import log
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
@@ -21,3 +40,79 @@ PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+CONFIG_PATH = "data/bot_conf.yaml"
+if os.name == "nt":
+    log.debug("Detected Windows, changing config path...")
+    CONFIG_PATH = os.getcwd() + "\\data\\bot_conf.yaml"
+
+if os.path.isfile(CONFIG_PATH):
+    log.info(CONFIG_PATH)
+    for item in (
+        data := yaml.load(open("data/bot_conf.yaml", "r"), Loader=yaml.CLoader)
+    ):
+        DEFAULTS[item.upper()] = data[item]
+else:
+    log.info("Using env vars")
+
+
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_int_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.int(name, default=default)) and not required:
+        log.warn("No int key: " + name)
+        return None
+    elif not data:
+        log.critical("No int key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_list_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.list(name, default=default)) and not required:
+        log.warn("No list key: " + name)
+        return []
+    elif not data:
+        log.critical("No list key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_bool_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.bool(name, default=default)) and not required:
+        log.warn("No bool key: " + name)
+        return False
+    elif not data:
+        log.critical("No bool key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
